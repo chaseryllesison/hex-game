077a560b6876fa628b5596c18afd9e567fc2bde3
Chasey Sison <chaseysison@Chaseys-MacBook-Air.local>
2021-11-24 02:12:26 +0800

    initialized landing page and game page

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
-// const path = require('path');
+const path = require('path');
 
 const port = 3000;
 
-// app.set('view engine', 'ejs');
-// app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.send('home');
-})
+    res.render('landingPage');
+});
+
+app.get('/game', (req, res) => {
+    res.render('game');
+});
+
 app.listen(port,() => {
     console.log(`serving on port ${port}`);
-})
+});
