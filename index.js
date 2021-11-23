d9fdd7cc53bd09dd8945da76ff76968106831798
Chasey Sison <chaseysison@Chaseys-MacBook-Air.local>
2021-11-24 02:29:28 +0800

    added layouts/boilerplate and set up ejs

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const ejsMate = require('ejs-mate');
 
 const port = 3000;
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
