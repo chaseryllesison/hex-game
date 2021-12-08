b55f3a129d17472d4f6d83d7a5df68419483de25
Chasey Sison <chaseysison@Chaseys-MacBook-Air.local>
2021-12-09 01:36:05 +0800

    added links to buttons

--- public/scripts/gameScript.js
+++ public/scripts/gameScript.js
@@ -1,14 +1,15 @@
-
-console.log(pickedColor);
-console.log(colors);
-
 var colorPads = document.querySelectorAll(".color-pad");
 
 colorPads.forEach(element => {
     element.onclick = () => {
         if(element.id == pickedColor) {
             document.querySelector("#color-indicator").style.backgroundColor = `#${pickedColor}`;
-            
+            colorPads.forEach(i => {
+                if(i.id != pickedColor){
+                    i.style.backgroundColor = `#${pickedColor}`;
+                }
+            })
+            document.querySelector("#popUp").style.visibility = "visible";
         } else {
             element.style.display = "none";
         }
